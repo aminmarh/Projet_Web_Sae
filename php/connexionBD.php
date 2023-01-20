c05304fbfe4ed21b87000e309de4ed66e82093b3
Amin Marheraoui <95435963+aminmarh@users.noreply.github.com>
2023-01-20 17:45:19 +0100

    V3 (Page contact fini)

---
+++ php/connexionBD.php
@@ -0,0 +1,17 @@
+<?php
+	$dbHostname = "localhost";
+	$dbName = "appli_sae";
+	$dbLogin = "root";	
+	$dbPwd = "root";
+	// URL de connexion à la base de données
+	$dbURL = "mysql:server=$dbHostname;dbname=$dbName";
+
+	// renvoie un objet Connexion ($pdo) à la base de données
+	function seConnecterBD () {
+		global $dbLogin,$dbPwd, $dbURL;		
+		// Créer un objet de connexion à la base de données
+		// Choix de ne pas traiter les exceptions (erreurs de connexion) ici mais au niveau de l'appelant
+		$pdo = new PDO ($dbURL, $dbLogin, $dbPwd);			
+		return $pdo;	
+	}
+?>
